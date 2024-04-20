485f7c35ea3b011898cf268014ebfe2d5e574103
Michalina <michalina.czechowska@skillforce.pl>
2024-04-20 14:26:20 +0200

    added xgboost gboosting svc

--- main.py
+++ main.py
@@ -24,25 +24,85 @@ data = pd.concat([data.drop(['Var_1'], axis=1), var_1_df], axis=1)
 
 # Label Encoding dla kolumn kategorycznych
 label_encoder = LabelEncoder()
-categorical_columns = ['Gender', 'Ever_Married', 'Graduated', 'Spending_Score']
+categorical_columns = ['Gender', 'Ever_Married', 'Graduated', 'Spending_Score', 'Segmentation']
 for col in categorical_columns:
     data[col] = label_encoder.fit_transform(data[col])
 
 # Podział danych na zbiór treningowy i testowy
 X = data.drop('Segmentation', axis=1)
 y = data['Segmentation']
+
+
+scaler = MinMaxScaler()
+X= scaler.fit_transform(X)
+
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Skalowanie danych
-scaler = MinMaxScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
 
+
+################################################################3
 # Inicjalizacja i dopasowanie modelu Random Forest
 model_rf = RandomForestClassifier(random_state=42)
-model_rf.fit(X_train_scaled, y_train)
+model_rf.fit(X_train, y_train)
 
 # Ocena modelu
-y_pred = model_rf.predict(X_test_scaled)
+y_pred = model_rf.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print("Dokładność modelu Random Forest:", accuracy)
+
+
+
+
+################################################################
+import xgboost as xgb
+xgb_clf = xgb.XGBClassifier(objective='multi:softmax', num_class=4, random_state=42)
+xgb_clf.fit(X_train, y_train)
+
+# Przewidywanie klas dla danych testowych
+y_pred = xgb_clf.predict(X_test)
+
+# Obliczenie dokładności modelu
+accuracy = accuracy_score(y_test, y_pred)
+print("Dokładność modelu XGBoost:", accuracy)
+
+
+
+#########################################3
+
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import accuracy_score
+
+# Inicjalizacja klasyfikatora Gradient Boosting
+gb_clf = GradientBoostingClassifier(random_state=42)
+
+# Dopasowanie modelu do danych treningowych
+gb_clf.fit(X_train, y_train)
+
+# Przewidywanie klas dla danych testowych
+y_pred_gb = gb_clf.predict(X_test)
+
+# Obliczenie dokładności modelu
+accuracy_gb = accuracy_score(y_test, y_pred_gb)
+print("Dokładność modelu Gradient Boosting:", accuracy_gb)
+
+
+
+########################################
+
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+# Inicjalizacja klasyfikatora SVM
+svm_clf = SVC(kernel='linear', decision_function_shape='ovr', random_state=42)
+
+# Dopasowanie modelu do danych treningowych
+svm_clf.fit(X_train, y_train)
+
+# Przewidywanie klas dla danych testowych
+y_pred_svm = svm_clf.predict(X_test)
+
+# Obliczenie dokładności modelu
+accuracy_svm = accuracy_score(y_test, y_pred_svm)
+print("Dokładność modelu SVM:", accuracy_svm)
