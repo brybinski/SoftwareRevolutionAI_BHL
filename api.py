ce0934ad4b10d0fdf52714a98ccd0631f1ea24c7
Bartosz Rybiński <bartoszrybinski@outlook.com>
2024-04-21 08:21:35 +0200

    cleaned repository code

--- api.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-# Tworzenie instancji aplikacji FastAPI
-app = FastAPI()
-
-# Definicja endpointu API
-@app.get("/")
-async def read_root():
-    return {"message": "Hello, World!"}
